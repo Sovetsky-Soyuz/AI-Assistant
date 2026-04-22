ff9b52d1d77467f4249e15398676f16f1b6836bd
Sovetsky-Soyuz <trannguyenminhquang02062003@gmail.com>
2026-04-22 22:12:22 +0700

    Delete online_test.py

--- online_test.py
+++
@@ -1,9 +0,0 @@
-import pymupdf4llm
-import pathlib
-
-md_text = pymupdf4llm.to_markdown("C:/Users/qt321/AI_Assistant/knowledge_base/High-Fidelity Simultaneous Speech-To-Speech Translation.pdf")
-
-md_result = pathlib.Path("C:/Users/qt321/AI_Assistant/knowledge_base/High-Fidelity Simultaneous Speech-To-Speech Translation.md").write_text(md_text, encoding="utf-8")
-
-
-# print(md_result)
